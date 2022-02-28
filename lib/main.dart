91baed40b0485e0863eb29170b97b0cbac935e81
Rudragit12 <rudrachaudhary180010@gmail.com>
2022-02-28 02:10:15 -0800

    commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_03/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(brightness: Brightness.dark),
+    );
+  }
+}
