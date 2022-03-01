121210c16d3379134406d729ae16b19719ca62d3
Rudragit12 <rudrachaudhary180010@gmail.com>
2022-02-28 20:05:04 -0800

    commit to day 3

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class Login_page extends StatelessWidget {
+  const Login_page({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Center(
-        child: Container(child: Text("This is home page")),
+        child: Text("Login page"),
       ),
     );
   }
